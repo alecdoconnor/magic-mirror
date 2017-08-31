dfe64ae801d67d932c7dfc1363a1159bfb9e4d33
Alec <alec@alecoconnor.com>
2017-08-31 04:38:18 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<link href="https://fonts.googleapis.com/css?family=Roboto:100" rel="stylesheet">
+<style>
+body {
+  background-color: #000000;
+  color: #ffffff;
+  font-family: 'Roboto', sans-serif;
+}
+</style>
+</head>
+<body>
+
+<h1>Magic Mirror</h1>
+
+<?php
+
+// echo "working".Date("Y");
+
+?>
+
+</body>
+</html>
